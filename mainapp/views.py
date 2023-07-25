c5fd316e013197b99748326f8221c60a4beeda74
CrasowMint <bivaetmail@gmail.com>
2023-07-26 00:27:23 +0300

    for cycle in html

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+import random
 
 
 class MainPageView(TemplateView):
@@ -10,11 +11,13 @@ class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
 
     def get_context_data(self, **kwargs):
-        #get data from TemplateView
+        # get data from TemplateView
         context = super().get_context_data(**kwargs)
-        context["news_title"] = "The best new title"
+        context["news_titles"] = []
         context["news_preview"] = "Description that attract everyone"
-        context["range"] = range(5)
+        for el in range(5):
+            rand_int = random.randint(1,20)
+            context["news_titles"].append(f"The best new title {rand_int}")
         return context
 
 class CoursesPageView(TemplateView):
