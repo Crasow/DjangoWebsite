428a82eead968c9bd26db6616ab42ae944a15b7b
CrsowMain <olariliya2003@gmail.com>
2023-08-21 00:10:18 +0300

    44

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from typing import Any, Dict
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
-import random
-from datetime import datetime
+from mainapp import models
+
 
 
 class MainPageView(TemplateView):
@@ -12,27 +13,35 @@ class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
 
     def get_context_data(self, **kwargs):
-        # get data from TemplateView
         context = super().get_context_data(**kwargs)
-
-        context["news_titles"] = "Loud news headline"
-        context["news_preview"] = "Description that attract everyone"
-        context["range"] = range(5)
-        context["datetime_obj"] = datetime.now()
-
-        # context["news_titles"] = []
-        # for el in range(5):
-        #     rand_int = random.randint(1,20)
-        #     context["news_titles"].append(f"The best new title {rand_int}")
+        context["news_qs"] = models.News.objects.all()[:5]
         return context
 
+class NewsPageDetailView(TemplateView):
+    template_name = "mainapp/news_detail.html"
+    
+    def get_context_data(self, pk=None, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(pk = pk, **kwargs)
+        context["news_objects"] = get_object_or_404(models.News, pk=pk)
+        return context
 
-class NewsWithPaginatorView(NewsPageView):
-    def get_context_data(self, page, **kwargs):
-        context = super().get_context_data(page=page, **kwargs)
-        context["page_num"] = page
+class CoursesListView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+    
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super(CoursesListView, self).get_context_data(**kwargs)
+        context["objects"] = models.Courses.objects.all()[:7]
         return context
     
+class CoursesDetailView(TemplateView):
+    template_name = "mainapp/courses_detail.html"
+    
+    def get_context_data(self, pk=None, **kwargs):
+        context = super(CoursesDetailView, self).get_context_data(**kwargs)
+        context["course_object"] = get_object_or_404(models.Courses, pk=pk)
+        context["lessons"] = models.Lesson.objects.filter(course=context["course_object"])
+        context["teachers"] = models.CourseTeachers.objects.filter(course=context["course_object"])
+        return context
 
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
